dbecdbbb075b2256c98cf0116de1eeb6d668f761
Hafiz-fish <hafizmrifqi@gmail.com>
2023-09-15 10:04:03 +0700

    Add Filament

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->bigInteger('phone')->nullable();
+            $table->string('city')->nullable();
+            $table->string('picture')->default('profile.png');
             $table->rememberToken();
             $table->timestamps();
         });
